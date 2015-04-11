1db1f2707788026da2a4eec3e1ea09726eda4626
unknown <jaclyncnguyen@utexas.edu>
2015-04-11 16:22:18 -0500

    added cumedist to tableau

---
+++ 01 Data/df.R
@@ -0,0 +1,1 @@
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from medcharge"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_jz7674',PASS='orcl_jz7674',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
